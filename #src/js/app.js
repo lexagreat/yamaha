84d775628d7c5cd26496023ed20643d5cb053fda
lexagreat <9608798010@mail.ru>
2024-08-15 18:52:15 +0300

    start catalog section

--- #src/js/app.js
+++ #src/js/app.js
@@ -77,6 +77,8 @@ function headerWork() {
 }
 
 function initHomeHeroSlider() {
+   const videos = document.querySelectorAll(".home-hero video");
+
    let slider = new Swiper(".home-hero .swiper", {
       navigation: {
          prevEl: ".home-hero .swiper-button-prev",
@@ -87,5 +89,28 @@ function initHomeHeroSlider() {
       },
       effect: "fade",
       speed: 1000,
+      on: {
+         slideChange(swiper) {
+            // https://metanit.com/web/html5/7.3.php // тут можно взять методы
+            videos.forEach((item) => {
+               item.currentTime = 0;
+               item.pause();
+            });
+            videos[swiper.activeIndex].play();
+         },
+      },
+   });
+   const soundBtn = document.querySelector(".home-hero__sound .btn");
+   soundBtn.addEventListener("click", () => {
+      let isMuted = videos[0].muted ? true : false;
+      videos.forEach((video) => {
+         if (isMuted) {
+            video.muted = false;
+            soundBtn.innerHTML = "Выключить звук";
+         } else {
+            video.muted = true;
+            soundBtn.innerHTML = "Включить звук";
+         }
+      });
    });
 }
