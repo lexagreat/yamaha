14cd0a5c3f819984789c58799394cf8a7560c06a
lexagreat <9608798010@mail.ru>
2024-08-20 17:21:12 +0300

    fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
    hoverOnCatalog();
    initProductSlider();
    productCardWork();
+   accordion(".garanty-item__header", ".garanty-item__spoiler");
 });
 
 function headerWork() {
@@ -269,4 +270,115 @@ function productCardWork() {
    toggleActions();
    hoverOnCart();
    preventRouteChange();
+}
+
+function accordion(linkSelector, contentSelector) {
+   // получаем линки
+   const openLinks = document.querySelectorAll(`${linkSelector}`);
+   // контенты
+   const contents = document.querySelectorAll(`${contentSelector}`);
+   if (openLinks.length > 0) {
+      for (let i = 0; i < openLinks.length; i++) {
+         let openLink = openLinks[i];
+         openLink.addEventListener("click", () => {
+            // все прячем
+            for (let j = 0; j < contents.length; j++) {
+               // если хоть один открывается - return
+               if (contents[j].classList.contains("collapsing")) {
+                  return;
+               } // Иначе
+               // все прячем
+               slideHide(contents[j]);
+            }
+            for (let j = 0; j < openLinks.length; j++) {
+               openLinks[j].classList.remove("active");
+            }
+            // записываем в переменную нужный таб
+            let content = openLink.nextElementSibling;
+            // работаем с классами линка
+            if (content.classList.contains("collapsing")) {
+               return;
+            } else if (content.classList.contains("collapse_show")) {
+               openLink.classList.remove("active");
+            } else {
+               openLink.classList.add("active");
+            }
+            // показываем нужный
+            slideShow(content);
+         });
+      }
+   }
+}
+
+function slideShow(el, duration = 500) {
+   // завершаем работу метода, если элемент содержит класс collapsing или collapse_show
+   if (
+      el.classList.contains("collapsing") ||
+      el.classList.contains("collapse_show")
+   ) {
+      return;
+   }
+   // удаляем класс collapse
+   el.classList.remove("collapse");
+   // сохраняем текущую высоту элемента в константу height (это значение понадобится ниже)
+   const height = el.offsetHeight;
+   // устанавливаем высоте значение 0
+   el.style["height"] = 0;
+   // не отображаем содержимое элемента, выходящее за его пределы
+   el.style["overflow"] = "hidden";
+   // создание анимации скольжения с помощью CSS свойства transition
+   el.style["transition"] = `height ${duration}ms ease`;
+   // добавляем класс collapsing
+   el.classList.add("collapsing");
+   // получим значение высоты (нам этого необходимо для того, чтобы просто заставить браузер выполнить перерасчет макета, т.к. он не сможет нам вернуть правильное значение высоты, если не сделает это)
+   el.offsetHeight;
+   // установим в качестве значения высоты значение, которое мы сохранили в константу height
+   el.style["height"] = `${height}px`;
+   // по истечении времени анимации this._duration
+   window.setTimeout(() => {
+      // удалим класс collapsing
+      el.classList.remove("collapsing");
+      // добавим классы collapse и collapse_show
+      el.classList.add("collapse");
+      el.classList.add("collapse_show");
+      // удалим свойства height, transition и overflow
+      el.style["height"] = "";
+      el.style["transition"] = "";
+      el.style["overflow"] = "";
+   }, duration);
+}
+function slideHide(el, duration = 500) {
+   // завершаем работу метода, если элемент содержит класс collapsing или collapse_show
+   if (
+      el.classList.contains("collapsing") ||
+      !el.classList.contains("collapse_show")
+   ) {
+      return;
+   }
+   // установим свойству height текущее значение высоты элемента
+   el.style["height"] = `${el.offsetHeight}px`;
+   // получим значение высоты
+   el.offsetHeight;
+   // установим CSS свойству height значение 0
+   el.style["height"] = 0;
+   // обрежем содержимое, выходящее за границы элемента
+   el.style["overflow"] = "hidden";
+   // добавим CSS свойство transition для осуществления перехода длительностью this._duration
+   el.style["transition"] = `height ${duration}ms ease`;
+   // удалим классы collapse и collapse_show
+   el.classList.remove("collapse");
+   el.classList.remove("collapse_show");
+   // добавим класс collapsing
+   el.classList.add("collapsing");
+   // после завершения времени анимации
+   window.setTimeout(() => {
+      // удалим класс collapsing
+      el.classList.remove("collapsing");
+      // добавим класс collapsing
+      el.classList.add("collapse");
+      // удалим свойства height, transition и overflow
+      el.style["height"] = "";
+      el.style["transition"] = "";
+      el.style["overflow"] = "";
+   }, duration);
 }
