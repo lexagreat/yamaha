e793f49a6b13bcedacb3d061b828b78dc6b2b05c
lexagreat <9608798010@mail.ru>
2024-10-31 14:39:33 +0300

    make pages

--- dist/js/app.js
+++ dist/js/app.js
@@ -1464,3 +1464,9 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+   anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      lenis.scrollTo(this.getAttribute("href"));
+   });
+});
